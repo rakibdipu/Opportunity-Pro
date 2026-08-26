5579a55e3a65c33484d0f654209db849ec79f5b8
rakibdipu <rakibdipu@users.noreply.github.com>
2026-08-26 18:34:57 +0600

    update config.js with live Render backend URL https://internradar-backend-4x63.onrender.com

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,9 @@
 const isLocal = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
 
 window.INTERNRADAR_CONFIG = {
-  apiBaseUrl: isLocal ? "http://127.0.0.1:8000" : "https://internradar-backend.onrender.com",
+  apiBaseUrl: isLocal ? "http://127.0.0.1:8000" : "https://internradar-backend-4x63.onrender.com",
   supabaseUrl: "https://vdtvbuhhzojbqtwxhtgx.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkdHZidWhoem9qYnF0d3hodGd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODEyNTYsImV4cCI6MjA5MTE1NzI1Nn0.nCPP70RIMvZouWIiNFoZJFjZ43q2bFlC_qlcd8FNCN0"
 };
+
 
